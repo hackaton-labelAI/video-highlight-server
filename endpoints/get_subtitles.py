34a4ad891b94c51568e447e5d0ae10b4ff28054d
KilArtur <kil_artur@inbox.ru>
2024-09-29 02:19:52 +0300

    update

--- endpoints/get_subtitles.py
+++ endpoints/get_subtitles.py
@@ -5,13 +5,13 @@ from fastapi import APIRouter, HTTPException
 
 
 @router.get("/project/{session_id}/subtitles")
-async def get_subtitles(session_id: str):
+async def get_subtitles(session_id: str, video_name: str):
     try:
         session_folder = f"session_info_{session_id}"
-        subtitles_path = os.path.join(session_folder, "subtitles.srt")
+        subtitles_path = os.path.join(session_folder, f"{video_name}.srt")
 
         if not os.path.exists(subtitles_path):
-            raise HTTPException(status_code=404, detail="Файл с субтитрами нет")
+            raise HTTPException(status_code=404, detail="Файл субтитров не найден")
 
         with open(subtitles_path, 'r', encoding='utf-8') as file:
             subtitles_content = file.read()
@@ -19,5 +19,5 @@ async def get_subtitles(session_id: str):
         return {"subtitles": subtitles_content}
 
     except Exception as e:
-        logging.error(f"Ошибка: {e}")
-        raise HTTPException(status_code=500, detail=f"Ошибка: {str(e)}")
+        logging.error(f"Ошибка получения субтитров: {e}")
+        raise HTTPException(status_code=500, detail=f"Ошибка получения субтитров: {str(e)}")
