efefe575f2f87b0fdb17292a18fa3de013db5c38
anasenicenkova <senichenkova02@inbox.ru>
2024-09-29 07:06:41 +0300

    large model

--- services/find_best.py
+++ services/find_best.py
@@ -206,7 +206,7 @@ async def fetch_completion(prompt: str, count = 0):
     try:
         res = await openai.chat.completions.create(
             messages=[{"role": "user", "content": prompt}],
-            model="just-ai/openai-proxy/gpt-4o-mini",
+            model="just-ai/openai-proxy/gpt-4o",
             temperature=0,
             response_format={"type": "json_object"},
             stream=False
